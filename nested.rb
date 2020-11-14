af4c9fab34c92c164ce9cbd9a4c9fb2f4e13db05
Wesley <wspencer3295@yahoo.com>
2020-11-14 00:32:12 -0700

    commit tree 72dd4bffaef24f612d5464c53d4de6c4c4a3ba30

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
+programmer_hash[:grace_hopper]
 
 
 end
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    programmer_hash[:dennis_ritchie][:languages].join
+
 end
 
 def adding_matz
@@ -79,8 +83,42 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+yukihiro_matsumoto = {known_for: "Ruby" , language: ["COBOL", "FORTRAN"]}
+programmer_hash.update programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+yukihiro_matsumoto = {known_for: "Ruby" , language: ["COBOL", "FORTRAN"]}
+programmer_hashprogrammer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+		 programmer_hash
 end
 
 def changing_alan
@@ -102,6 +140,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
      
 end
 
@@ -121,5 +161,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
